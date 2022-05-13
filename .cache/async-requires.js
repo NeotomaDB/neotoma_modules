69213cc4d92db6c19e25697abcfeeb8e47050a58
Socorro <s.dominguez@ht-data.com>
2022-05-13 07:38:19 -0700

    removed cache

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,7 @@
 const preferDefault = m => (m && m.default) || m
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("./../../../node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-templates-chapter-js": () => import("./../../../src/templates/chapter.js" /* webpackChunkName: "component---src-templates-chapter-js" */)
 }
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/haritoto/Desktop/UWisconsin/neotoma/neotoma_workshop/node_modules/gatsby-plugin-offline/app-shell.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/haritoto/Desktop/UWisconsin/neotoma/neotoma_workshop/.cache/dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/haritoto/Desktop/UWisconsin/neotoma/neotoma_workshop/src/pages/index.js"))),
   "component---src-templates-chapter-js": hot(preferDefault(require("/Users/haritoto/Desktop/UWisconsin/neotoma/neotoma_workshop/src/templates/chapter.js")))
 }
